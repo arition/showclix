6358dd14b875b243c37c75dbeb65c131253e1b7b
Kana Minami <admin@gmail.com>
2018-05-12 14:34:52 +0800

    fastpoll; test types of page

--- inject.js
+++ inject.js
@@ -1,19 +1,118 @@
-if (document.querySelector("body").innerHTML.includes("Tickets go on sale soon.") ||
-    document.querySelector("body").innerHTML.includes("You can access this event page multiple times")) {
-    location.reload();
+window.addEventListener("load", function() {
+  var match = null;
+
+  var event_re = new RegExp('^/event/[^/]+$');
+  match = event_re.exec(window.location.pathname);
+  if (match) {
+    // We are in event page now
+    try_reload_event_page();
+    fastPoll();
+    var click_interval = window.setInterval(function () {
+      if (click_select_seats()) {
+        window.clearInterval(click_interval);
+      }
+    }, 1000);
+  }
+
+  var pyos_re = new RegExp('^/event/[^/]+/pyos$');
+  match = pyos_re.exec(window.location.pathname);
+  if (match) {
+    // We are in reservation page now
+    var select_seat_url = "https://api.seats.io/system/public/6ee8c1fa-4bac-4ed0-89aa-26b6228f06fa/seasons/actions/hold-objects";
+    var post_data = {};
+    var ticket_url = "https://www.showclix.com/areservation/a7b5dd1a900df0a1526106187554ef05647827f1/tickets?captured_via=online";
+    var payload = {"tickets":[{"event_id":"4725977","event_seat_ids":["uuid13941"],"data":{"reserve_type":"Seat"}}],"seatsioReservationToken":"220ff018-b746-4c39-b33d-73112f97a732","category":"ORCHESTRA"}
+   
+    var iframe_interval = window.setInterval(function () {
+      var iframe = document.querySelector("#seatsio-seating-chart > iframe");
+      if (iframe) {
+        // TODO: We need to insert code into the iframe
+        // Maybe we can write a separate content script
+        //var innerDoc = iframe.contentDocument || iframe.contentWindow.document;
+        iframe.addEventListener("load", function () {
+          //var canvas = innerDoc.querySelector("#chartContainer > canvas");
+          //console.log(canvas);
+        });
+        window.clearInterval(iframe_interval);
+      }
+    }, 1000);
+  }
+});
+
+function try_reload_event_page() {
+  if (document.querySelector("body").innerHTML.includes("Tickets go on sale soon.") ||
+      document.querySelector("body").innerHTML.includes("You can access this event page multiple times")) {
+      location.reload();
+  }
+  if (document.querySelector("body").innerHTML.includes("You are unable to access the checkout process. In most cases, this happens due to one of the following actions:")) {
+      chrome.runtime.sendMessage({
+          action: "deleteCookie"
+      }, function (response) {
+          console.log("delete Cookie success.");
+          location.reload();
+      });
+  }
 }
-if (document.querySelector("body").innerHTML.includes("You are unable to access the checkout process. In most cases, this happens due to one of the following actions:")) {
-    chrome.runtime.sendMessage({
-        action: "deleteCookie"
-    }, function (response) {
-        console.log("delete Cookie success.");
-        location.reload();
-    });
+  
+function click_select_seats() {
+  var submit = document.querySelector("#submit_pyos_request_2");
+  if (submit != null) {
+    console.log("click_select_seats");
+    submit.click();
+    return true;
+    /*if (document.querySelector("#pyos_request") != null) {*/
+      //document.querySelector("#pyos_request").remove();
+    //}
+    //document.querySelector("#ticket-form").insertAdjacentHTML('beforeend', '<input type="hidden" name="pyos_request" id="pyos_request" value="1" />');
+    /*document.querySelector("#ticket-form").submit();*/
+  }
+  return false;
 }
-if (document.querySelector("#submit_pyos_request_2") != null) {
-    if (document.querySelector("#pyos_request") != null) {
-        document.querySelector("#pyos_request").remove();
+
+function fastPoll() {
+  var user_token = null;
+  var re = new RegExp('var user_token = "([^"]*)";');
+  var scripts = document.querySelectorAll("script");
+  for (var i=0; i < scripts.length; i++) {
+    var s = scripts[i];
+    var html = s.innerHTML;
+    var match = re.exec(html);
+    if (match !== null) {
+      user_token = match[1];
+      console.log(user_token);
+      break;
     }
-    document.querySelector("#ticket-form").insertAdjacentHTML('beforeend', '<input type="hidden" name="pyos_request" id="pyos_request" value="1" />');
-    document.querySelector("#ticket-form").submit();
+  }
+
+  var event_url = window.location.pathname;
+  var poll_url = event_url + '/poll.php';
+  var pollTime = 1E3;
+  if (user_token !== null) {
+    window.setInterval(function() {
+      pollServer(poll_url, user_token);
+    }, pollTime);
+    var s = document.createElement("script");
+    s.innerHTML = "function pollServer() { console.log(\"customPollServer()!\"); }";
+    document.head.appendChild(s);
+  }
+}
+
+function pollServer(url, user_token){
+    $.get(url, {
+        token : user_token,
+        tag   : undefined
+    }, function (a) {
+        a = jQuery.parseJSON(a);
+        if (a.status===1) reloadPage();
+        var b = $("#alert").text().length;
+        if (a.message.length===0) $("#alert").fadeOut("slow");
+        else if (a.message.length !==0 && a.message !==$("#alert").text()) {
+            if (b>0)$("#alert").fadeOut("slow",function(){$("#alert").fadeIn("slow").text(a.message);});
+            else {$("#alert").fadeIn("slow");}
+        }
+    })
+}
+
+function reloadPage(){
+    window.location.reload(true)
 }
