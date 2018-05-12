bdf444dca3fd867e4cceebb07e6d2b866e2187c4
arition <aritionkb@gmail.com>
2018-05-11 19:53:26 -0700

    update

--- background.js
+++ background.js
@@ -4,9 +4,21 @@ function deleteCookie() {
     }, function (cookies) {
         for (var i = 0; i < cookies.length; i++) {
             chrome.cookies.remove({
-                url: "http://www.showclix.com" + cookies[i].path,
+                url: "https://www.showclix.com" + cookies[i].path,
                 name: cookies[i].name
             });
+            console.log('removed ' + cookies[i].name + ' in ' + "www.showclix.com" + cookies[i].path)
+        }
+    });
+    chrome.cookies.getAll({
+        domain: ".showclix.com"
+    }, function (cookies) {
+        for (var i = 0; i < cookies.length; i++) {
+            chrome.cookies.remove({
+                url: "https://www.showclix.com" + cookies[i].path,
+                name: cookies[i].name
+            });
+            console.log('removed ' + cookies[i].name + ' in ' + ".showclix.com" + cookies[i].path)
         }
     });
 }
--- inject.js
+++ inject.js
@@ -1,9 +1,12 @@
-if(document.querySelector("body").innerHTML.includes("Tickets go on sale soon.")){
+if (document.querySelector("body").innerHTML.includes("Tickets go on sale soon.") ||
+    document.querySelector("body").innerHTML.includes("You can access this event page multiple times")) {
     location.reload();
 }
-if(document.querySelector("body").innerHTML.includes("You are unable to access the checkout process. In most cases, this happens due to one of the following actions:")){
-    chrome.runtime.sendMessage({action: "deleteCookie"}, function(response) {
+if (document.querySelector("body").innerHTML.includes("You are unable to access the checkout process. In most cases, this happens due to one of the following actions:")) {
+    chrome.runtime.sendMessage({
+        action: "deleteCookie"
+    }, function (response) {
         console.log("delete Cookie success.");
         location.reload();
-      });
+    });
 }
